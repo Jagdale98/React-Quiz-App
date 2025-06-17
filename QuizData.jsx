54e09410a86b50312bb0ae1c05fbcb50117670e7
Pooja Bagal <poojabagal44@gmail.com>
2025-06-17 15:18:51 +0530

    Add files via upload

---
+++ QuizData.jsx
@@ -0,0 +1,77 @@
+export const QuizData = [
+    {
+        question: "Which language runs in a web browser?",
+        options:[ "Java", "C", "Python", "JavaScript"],
+        answer: 4
+    },
+    {
+        question: "What does CSS stand for?",
+        options:["Central Style Sheets", "Cascading Style Sheets", "Cascading Simple Sheets", "Cars SUVs Sailboats"],
+        answer: 2
+    },
+    {
+        question:"Which of the following is correct about JavaScript?",
+        options:["JavaScript is an Object-Based language","JavaScript is Assembly-language","JavaScript is an Object-Oriented language","JavaScript is a High-level language"],
+        answer:1
+    }, 
+    {
+        question: "What does HTML stand for?",
+        options: ["Hypertext Markup Language", "Hypertext Markdown Language", "Hyperloop Machine Language", "Helicopters Terminals Motorboats Lamborginis"],
+        answer: 1
+    },
+    {
+        question: "What year was JavaScript launched?",
+        options: ["1996", "1995", "1994", "none of the above"],
+        answer:2
+    },
+    {
+        question : "Which of the following command is used to install create-react-app?",
+        options : ["npm install -g create-react-app","npm install create-react-app","npm install -f create-react-app","install -g create-react-app"],
+        answer : 1
+    },
+    {
+        question : "What of the following is used in React.js to increase performance",
+        options:["Original DOM","Virtual DOM","Both A and B.","None of the above"],
+        answer: 2
+    },
+    {
+        question : "Which of the following acts as the input of a class-based component?",
+        options:["Class","Factory","Rende","Props"],
+        answer:4
+    },
+    {
+        question:"What is JavaScript?",
+        options:["JavaScript is a scripting language used to make the website interactive","JavaScript is an assembly language used to make the website interactive","JavaScript is a compiled language used to make the website interactive","None of the mentioned"],
+        answer:1
+    },
+    {
+        question : "What is the default port where webpack-server runs?",
+        options:["3000","8080","3030","6060"],
+        answer:2
+    },
+    {
+        question : "How many ways of defining your variables in ES6?",
+        options:["1","3","4","5"],
+        answer:2
+    },
+    {
+        question:"Which of the following can be used to call a JavaScript Code Snippet?",
+        options:["Function/Method","Preprocessor","Triggering Event"," RMI"],
+        answer:1
+    },
+    {
+        question:"Which of the following option is correct in the case of the Babel?",
+        options:["Babel is a Compiler.","Babel is a Transpilar.","None of the above.","Both A and B are correct."],
+        answer:4
+    },
+    {
+        question:" Which of the following is used to pass data to a component from outside in React.js?",
+        options:["SetState","Render with arguments","Props","PropTypes"],
+        answer:3
+    },
+    {
+        question:"What does ES6 stand for?",
+        options:["ECMAScript 6","ECMA 6","ECMAJavaScript 6","EJavaScript 6"],
+        answer:1
+    }
+];
